54ebda527156bbfa98f6ef4e1ef6883a0e39e160
visaka <visidevi@gmail.com>
2017-11-08 17:53:46 -0300

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,42 @@
-alert('work!');
+
+//FUNCION AL HACER CLICK
 function add(){
-	
-	var comments = document.getElementById('comment').value;
+
 	/*creo una variable , rescato el comentario y lo agrego a valor*/
-	
-	/*validar espacio vacio*/
-	if (comments.length == 0){
-		alert('Comentario Vacio \n ingresa un comentario Valido.')
-	}else{
-		
-	/*limpio placeHOlder*/
-	document.getElementById('comment').value="";
+	var comments = document.getElementById('comment').value;	
+	/*validar espacio vacio , */
+	if(comments==null || comments.length==0 || comments.length >140 ){
+    alert(" ERROR!! Ingresa un comentario Valido");
+    return false;
+  	} //else if ( document.getElementById("count").value <= 0 ){
+  		//alert('hola')
+
+  	//CONTADOR limpio,   /*limpio placeHOlder*/
+  	document.getElementById('comment').value="";
+  	var num = document.getElementById("count");
+   	num.value= 140;
+   	num.style.color = "black";
+
 
 //Cre un div donde van a estar mis twt
 	var newComments = document.createElement('div');
 	var cont = document.getElementById('cont');
-	/*creando input de punto checkbox*/
-		/*creando parrafo*/
+	/* parrafo*/
 	var paragraph = document.createElement('p');
 	/*agregando classe*/
 	paragraph.classList.add('tweet');
-	 var nodoText=document.createTextNode(comments);
+	var nodoText=document.createTextNode(comments);
 	 /*AÑADIR NODO DE TEXTO AL ELEMENTO*/
 	 paragraph.appendChild(nodoText);
-
-
-
 /*ESTOS ELEMENTOS VAN A EL DIV newComments */
-
-
 	var trash=document.createElement('i');
 	/* diciendo que trash es una clase*/
 	trash.classList.add('fa','fa-trash','trash');
-
-	/*agregamos los hijos al padre o caja de comentarios*/
+	/*agregamos los hijos al la caja de comentarios*/
 	newComments.appendChild(paragraph);
 	newComments.appendChild(trash);
-
 	/*pasar todo al lo creado a el div cont*/
-
 	cont.appendChild(newComments);
-
 	/*vamos a hacer un esuchador CON ADD EVENT LISTENER*//*un EVENTO*/
 	/*eliminar un comentario*/
 	/*llama a cont luego le elimina todo el comentario alojado en newComments*/
@@ -48,7 +44,23 @@ function add(){
 	trash.addEventListener('click',function(){
 		alert('Deseas elimar este comentario?')
 		cont.removeChild(newComments);
-	})
+	});
+	
+};
 
-	}
-}
+//CONTADOR 
+	var contador2= function(comment,counter) { 
+        counter.value = 140 - comment.value.length;
+		if (counter.value > 80 && counter.value <=140) {
+		   counter.style.color = "#2980B9"
+		 }else if (counter.value > 40 && counter.value <=80) {
+		   counter.style.color = "#7FB3D5"
+		 }else if (counter.value > 0 && counter.value <=40) {
+		   counter.style.color = "#A9CCE3"
+		 }else{
+		   counter.style.color = "#D4E6F1";
+		 }
+
+	};
+
+	
